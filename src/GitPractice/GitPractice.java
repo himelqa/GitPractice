add7478fe9b2a2d9b792db1bbfd42e99b3c5948e
Ahmad Al Multajim <multajim@gmail.com>
2022-02-15 19:31:29 -0500

    I changed GitPractice

--- src/GitPractice/GitPractice.java
+++ src/GitPractice/GitPractice.java
@@ -5,7 +5,7 @@ public class GitPractice {
 	public static void main(String[] args) {
 		System.out.println("My Name is Telentech");
 		System.out.println("My name Telentech2");
-
+		System.out.println("My Name Mahir");
 	}
 
 }
